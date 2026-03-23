c25ad9a15ffbcf7b6b0c2a1a332da32f56b108c7
hansraj3160 <hansraj.bcsf18@iba-suk.edu.pk>
2026-03-23 11:51:35 +0530

    cors added

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 // index.js
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const users_routes=require('./app/routes/users_route');
 const tasks_routes=require('./app/routes/tasks_route');
 const payment_routes=require('./app/routes/payment_route');
+
+const defaultAllowedOrigins = [
+  'https://batprox.com',
+  'https://www.batprox.com',
+  'http://batprox.com',
+  'http://www.batprox.com'
+];
+
+const envAllowedOrigins = (process.env.CORS_ALLOWED_ORIGINS || '')
+  .split(',')
+  .map((origin) => origin.trim())
+  .filter(Boolean);
+
+const allowedOrigins = Array.from(new Set([...defaultAllowedOrigins, ...envAllowedOrigins]));
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204
+};
 // Middleware (optional)
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
